87563afdec24866b871cf88e99591163fe597216
thelastpenguin212 <thelastpenguin212@gmail.com>
2012-12-13 08:30:57 +0000

    More work on menu system.

--- gamemode/vgui/main.lua
+++ gamemode/vgui/main.lua
@@ -11,21 +11,7 @@
 
 if(CLIENT)then
 	local function ClientMenu()
-		if( not NRP.mainmenu or not ValidPanel( NRP.mainmenu ) )then
-			NRP.MakeMainMenu()
-		end
-		if( ValidPanel( NRP.mainmenu ) )then
-			if( NRP.mainmenu:IsVisible()) then
-				NRP.mainmenu:OnClose( )
-				return
-			end
-			print("Showing menu!")
-			NRP.mainmenu:MoveTo( ScrW() / 4, ScrH() / 4, 0.5, 0, 1, nil )
-			NRP.mainmenu:SetVisible( true )
-			NRP.mainmenu:MakePopup()
-		end
-		NRP.UpdateMenuTabs( )
-		hook.Call("NeoRP_MenuOpened",GAMEMODE, menu )
+		NRP.OpenMainMenu()
 	end
 	concommand.Add("NRP_Menu",function( ply, cmd, args )
 		ClientMenu()
--- gamemode/vgui/tab_jobs.lua
+++ gamemode/vgui/tab_jobs.lua
@@ -11,8 +11,38 @@
 local JOBTAB = {}
 JOBTAB.name = "Jobs"
 JOBTAB.icon = "icon16/user_red.png"
+
+local drawFill = function( panel ) -- custom look for the Property Sheet Tabs.
+	surface.SetDrawColor( panel.fillColor or Color( 255, 255, 255 ) )
+	local x,y = panel:GetPos()
+	local w, h = panel:GetSize()
+	surface.DrawRect( 0, 0, w, h )
+end
+
+
 JOBTAB.make = function( panel )
+	local w, h = panel:GetWide(), panel:GetTall()
+	print( w .. ', '..  h )
+	local dlist = vgui.Create( "DPanelList", panel )
+	dlist:SetPos( 5, 5 )
+	dlist:SetSize( w / 2 - 3, h - 10)
+	dlist:SetSpacing( 5 )
+	dlist:EnableHorizontal( false )
+	dlist:EnableVerticalScrollbar( true )
+	panel.dlist = dlist
 	
+	local teams = NRP.GetAllTeams()
+	local vteams = {} -- table of team VGUI panels.
+	
+	for k,v in pairs( teams )do
+		local p = vgui.Create( "DPanel", panel )
+		--p.fillColor = Color( 155, 155, 155 )
+		--p.Paint = drawFill
+		dlist:AddItem( p )
+		table.insert( vteams, p )
+	end
+	
+	panel.vteams = vteams
 end
 JOBTAB.update = function( panel )
 	
