5fdf960db132719a182e2edbe28c21c1fe8e16a8
thelastpenguin212@gmail.com <thelastpenguin212@gmail.com@9e640d3e-6fd7-0db3-b4f9-1bd825bbcd1b>
2012-12-30 19:22:44 +0000

    Added new entities: money_printer and scrap_metal (a detail entity)

---
+++ trunk/entities/entities/scrap_metal/init.lua
@@ -0,0 +1,42 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+
+
+function ENT:Initialize()
+	self:SetModel("models/gibs/metal_gib" .. math.random(1,5) .. ".mdl")
+	
+	self.Entity:PhysicsInit(SOLID_VPHYSICS)
+	self.Entity:SetMoveType(MOVETYPE_VPHYSICS)
+	self.Entity:SetSolid(SOLID_VPHYSICS)
+	self:SetUseType(SIMPLE_USE)
+	
+	local phys = self.Entity:GetPhysicsObject()
+	self.nodupe = true
+	self.ShareGravgun = true
+	
+	if(self:IsOnFire())then self:Extinguish() end
+	
+	if phys and phys:IsValid() then phys:Wake() end
+end
+
+
+function ENT:Use(activator,caller)
+	local amount = self.dt.amount
+
+	activator:AddMoney(amount or 0)
+	GAMEMODE:Notify(activator, 1, 4, "You melt down and sell scrapmetal for $"..amount)
+	self:Remove()
+end
+
+function DarkRPCreateScrapMetal(pos, amount)
+	local scrap = ents.Create("scrap_metal")
+	scrap:SetPos(pos)
+	scrap.dt.amount = amount
+	scrap:Spawn()
+	scrap:Activate()
+	return scrap
+end
+	
